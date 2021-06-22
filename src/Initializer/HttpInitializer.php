d8a993c8ef512129cf0525471a967e39225cdc6d
MieszkoMalawski <mieszko.malawski@miinto.com>
2021-06-22 09:13:03 +0200

    fix dynamic call in static method

--- src/Initializer/HttpInitializer.php
+++ src/Initializer/HttpInitializer.php
@@ -3,7 +3,6 @@
 
 namespace Maleficarum\ContextTracing\Initializer;
 
-
 use Maleficarum\ContextTracing\Carrier\Http\HttpHeader;
 use Maleficarum\ContextTracing\ContextTracker;
 use Maleficarum\ContextTracing\SimpleTracer;
@@ -14,7 +13,7 @@ class HttpInitializer
     static public function initialize(array $opts = [])
     {
         $tracer = new SimpleTracer();
-        (new HttpHeader())->extract($tracer, $this->getAllHeaders());
+        (new HttpHeader())->extract($tracer, self::getAllHeaders());
         $id = TrackingId::RID($opts['']);
         if (!$tracer->hasItem(SimpleTracer::MASTER_ID)) {
             $tracer->addItem(SimpleTracer::MASTER_ID, $id->generate());
@@ -23,7 +22,7 @@ class HttpInitializer
         ContextTracker::init($tracer);
     }
 
-    private function getAllHeaders()
+    static private function getAllHeaders()
     {
         $headers = [];
         foreach ($_SERVER as $name => $value) {
