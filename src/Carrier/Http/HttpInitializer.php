aac5a55fe71948fb1d29cd9f4c10bc85fbf0cc98
MieszkoMalawski <mieszko.malawski@miinto.com>
2021-08-27 15:45:52 +0200

    split initializers

---
+++ src/Carrier/Http/HttpInitializer.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Maleficarum\ContextTracing\Carrier\Http;
+
+
+use Maleficarum\ContextTracing\ContextTracker;
+use Maleficarum\ContextTracing\Identifiers\TrackingId;
+use Maleficarum\ContextTracing\SimpleTracer;
+
+class HttpInitializer
+{
+    /**
+     * @param array $headers
+     * @param string $serviceName
+     */
+    static public function initialize(array $headers, $serviceName)
+    {
+        $tracer = new SimpleTracer();
+        (new HttpHeader())->extract($tracer, $headers);
+        $id = TrackingId::RID($serviceName);
+        if (!$tracer->hasItem(SimpleTracer::MASTER_ID)) {
+            $tracer->addItem(SimpleTracer::MASTER_ID, $id->generate());
+        }
+        $tracer->addItem(SimpleTracer::CURRENT_ID, $id->generate());
+        ContextTracker::init($tracer);
+    }
+}
--- src/Initializer/HttpInitializer.php
+++ src/Initializer/HttpInitializer.php
@@ -3,25 +3,14 @@
 
 namespace Maleficarum\ContextTracing\Initializer;
 
-use Maleficarum\ContextTracing\Carrier\Http\HttpHeader;
-use Maleficarum\ContextTracing\Carrier\Http\HttpHeaderProvider;
-use Maleficarum\ContextTracing\ContextTracker;
-use Maleficarum\ContextTracing\SimpleTracer;
-use Maleficarum\ContextTracing\Identifiers\TrackingId;
-
 class HttpInitializer
 {
     static public function initialize(array $opts = [])
     {
-        $tracer = new SimpleTracer();
-        /** @var HttpHeaderProvider $provider */
-        $provider = $opts['http_header_provider'];
-        (new HttpHeader())->extract($tracer, $provider->getHeaders());
-        $id = TrackingId::RID($opts['context.service_name']);
-        if (!$tracer->hasItem(SimpleTracer::MASTER_ID)) {
-            $tracer->addItem(SimpleTracer::MASTER_ID, $id->generate());
-        }
-        $tracer->addItem(SimpleTracer::CURRENT_ID, $id->generate());
-        ContextTracker::init($tracer);
+        $app = $opts['app'];
+        \Maleficarum\ContextTracing\Carrier\Http\HttpInitializer::initialize(
+            $app->request->getHeaders(),
+            array_key_exists('prefix', $opts) ? $opts['prefix'] : 'service'
+        );
     }
 }
